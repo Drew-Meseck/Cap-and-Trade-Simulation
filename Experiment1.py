becf22ff9423896ee6f117b7ead8638f53a488aa
Drew-Meseck <42789729+Drew-Meseck@users.noreply.github.com>
2020-11-15 14:19:57 -0700

    Added some experiment code
    Updated requirements.txt

---
+++ Experiment1.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+from os import cpu_count
+from multiprocessing import Pool 
+from model import Environment
+
+
+
+def run_experiment(env):
+    pass
+
+#This 
+def main():
+    cpus = cpu_count()
+    
+
+if __name__ == "__main__":
+    main()
